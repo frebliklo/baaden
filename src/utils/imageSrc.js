44353065e63d54f82140e5dd24bf796e2e446f57
Frederik Klovborg <klovborg@gmail.com>
2018-11-20 21:53:53 +0100

    fix cdn url

--- src/utils/imageSrc.js
+++ src/utils/imageSrc.js
@@ -9,12 +9,19 @@ export const getCardSrc = image => {
   const imageBaseUrl = `${image}-/format/auto/-/preview/`
   const baseWidth = 448
   const baseHeight = 264
-  return `${imageBaseUrl}${baseWidth*2}x${baseHeight*2}`
+  return `${imageBaseUrl}${baseWidth*2}x${baseHeight*2}/`
 }
 
 export const getImgSrcSet = image => {
-  const imageBaseUrl = `${image}-/format/auto/-/scale-crop/`
+  const imageBaseUrl = `${image}-/format/auto/-/scale_crop/`
   const baseWidth = 930
   const baseHeight = 700
   return `${imageBaseUrl}${baseWidth}x${baseHeight}/, ${imageBaseUrl}${baseWidth*1.5}x${baseHeight*1.5}/ 1.5x, ${imageBaseUrl}${baseWidth*2}x${baseHeight*2}/ 2x, ${imageBaseUrl}${baseWidth*3}x${baseHeight*3}/ 3x`
+}
+
+export const getImgSrc = image => {
+  const imageBaseUrl = `${image}-/format/auto/-/scale_crop/`
+  const baseWidth = 1860
+  const baseHeight = 1400
+  return `${imageBaseUrl}${baseWidth*2}x${baseHeight*2}/`
 }
