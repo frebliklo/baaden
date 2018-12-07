8aae4efa547ab07985386a7b4af9c3e56a0d4cd7
Frederik Klovborg <klovborg@gmail.com>
2018-12-07 18:11:02 +0100

    test favicon

--- static.config.js
+++ static.config.js
@@ -102,12 +102,12 @@ export default {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content={theme.colors.blueDark} />
-        <link rel="apple-touch-icon" sizes="180x180" href={require('./src/assets/favicon/apple-touch-icon.png')} />
-        <link rel="touch-icon" sizes="180x180" href={require('./src/assets/favicon/touch-icon.png')} />
-        <link rel="icon" type="image/png" sizes="32x32" href={require('./src/assets/favicon/favicon-32x32.png')} />
-        <link rel="icon" type="image/png" sizes="16x16" href={require('./src/assets/favicon/favicon-16x16.png')} />
-        <link rel="mask-icon" href={require('./src/assets/favicon/safari-pinned-tab.svg')} color={theme.colors.blueDark} />
-        <link rel="shortcut icon" href={require('./src/assets/favicon/favicon-16x16.png')} />
+        <link rel="apple-touch-icon" sizes="180x180" href="src/assets/favicon/apple-touch-icon.png" />
+        <link rel="touch-icon" sizes="180x180" href="src/assets/favicon/touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="src/assets/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="src/assets/favicon/favicon-16x16.png" />
+        <link rel="mask-icon" href="src/assets/favicon/safari-pinned-tab.svg" color={theme.colors.blueDark} />
+        <link rel="shortcut icon" href="src/assets/favicon/favicon-16x16.png" />
         <link href="https://fonts.googleapis.com/css?family=Catamaran:400,500,700,800,900" rel="stylesheet" />
         {renderMeta.styleTags}
       </Head>
